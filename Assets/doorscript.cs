791af74513d56d4b6fd5c78f04fc48a1c6b34715
jcianci12 <jcianci12@gmail.com>
2022-09-07 20:35:44 +1000

    update to looks and modeling

---
+++ Assets/doorscript.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class doorscript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        //get the door parent object
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
