5b59e7f27443a79ce1ca983aa1eb46839e71bb3a
jcianci12 <jcianci12@gmail.com>
2022-11-26 13:12:31 +1000

    gun ejection point

--- Assets/Prefabs/Gun/pressure plate/GunPressurePlate.cs
+++ Assets/Prefabs/Gun/pressure plate/GunPressurePlate.cs
@@ -10,7 +10,8 @@ public class GunPressurePlate : MonoBehaviour
     //[SerializeField]
     //GameObject gun;
     public static GameObject playerobject;
-    public GunInputHandler gunInputHandler ;
+    public GunInputHandler gunInputHandler;
+    public GameObject ejectionPoint;
     private Vector3 move;
     public GameObject gun;
     GameObject cube;
@@ -20,32 +21,19 @@ public class GunPressurePlate : MonoBehaviour
     }
     public void Update()
     {
-        if (transform.childCount > 1)
-        {
-foreach(var o in gameObject.GetComponentsInChildren<PlayerController>())
-        {
-            //Debug.Log(o.name);
-                gun.transform.forward = move;
-                gun.transform.Rotate(Vector3.up,-90);  
-            }
-        }
-        
+        gun.transform.forward = move;
+        gun.transform.Rotate(Vector3.up, -90);
 
-        //if (gameObject.GetComponentInChildren<PlayerController>()!=null &&  move != Vector3.zero)
-        //{
-        //    gun.transform.forward = move;
-        //    gun.transform.Rotate(Vector3.up,-90);            
-        //}
     }
     public void OnMove(InputAction.CallbackContext context)
     {
-        Vector2 movement = context.ReadValue<Vector2>();       
-       
-
-        move = Camera.main.GetComponent<CameraRelativeMovement>().GetCameraRelativeMovement(
+        Vector2 movement = context.ReadValue<Vector2>();
+        
+            move = Camera.main.GetComponent<CameraRelativeMovement>().GetCameraRelativeMovement(
             new Vector3(movement.y, -movement.x, 0)
             );
-        Debug.Log(move);
+            Debug.Log(move);
+        
     }
     public void AttachPlayer(GameObject player)
     {
@@ -53,8 +41,8 @@ foreach(var o in gameObject.GetComponentsInChildren<PlayerController>())
         if (p)
         {
             player.transform.parent = transform;
-            p.MovementActive = false;            
-        }        
+            p.MovementActive = false;
+        }
     }
     public void DetachPlayer(GameObject player)
     {
@@ -72,13 +60,13 @@ foreach(var o in gameObject.GetComponentsInChildren<PlayerController>())
         Debug.Log("fire!");
         //rotate the projectile 90 degrees on the x
         Quaternion rotation = gun.transform.rotation;
-        
-        GameObject ball = Instantiate(projectile, gun.transform.position + gun.transform.forward *2f,
-                                                     gun.transform.rotation);
+
+        GameObject ball = Instantiate(projectile, ejectionPoint.transform.position ,
+                                                     ejectionPoint.transform.rotation);
         ball.transform.Rotate(90, 0, 0);
         ball.GetComponent<Rigidbody>().AddRelativeForce(new Vector3
                                              (0, 0, launchVelocity));
-        
+
 
     }
 
