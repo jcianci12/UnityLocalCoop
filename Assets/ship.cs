2493c54d65b407d67bce2881f02e237d8687a8bf
jcianci12 <jcianci12@gmail.com>
2022-12-31 20:05:19 +1000

    level adjustments working

--- Assets/ship.cs
+++ Assets/ship.cs
@@ -1,3 +1,4 @@
+using ProjectDawn.SplitScreen;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -8,6 +9,10 @@ public class ship : MonoBehaviour
     Rigidbody rb;
     public Slider health;
     public GameObject explosion;
+    public Transform explosionOrigin;
+    public float explosionForce;
+    public GameObject explosionParent;
+    public GameObject splitScreenGameObject;
 
 
     // Start is called before the first frame update
@@ -36,9 +41,37 @@ public class ship : MonoBehaviour
     }
     private void Destroy()
     {
-        Instantiate(explosion, gameObject.transform.position, gameObject.transform.rotation);
 
-        Destroy(gameObject);
+        //we need to unparent the split screen prior to destroying the ship so
+        //that we still have a cam rendering.
+        UnparentSplitScreen();
 
+        //Instantiate(explosion, gameObject.transform.position, gameObject.transform.rotation);
+        //var rbs = explosionParent.GetComponentsInChildren<MeshCollider>();
+        //for (var i = 0; i < rbs.Length; i++)
+        //{
+        //    //UnityEngine.Gizmos.DrawSphere(gameObject.transform.position, 50);
+        //    rbs[i].gameObject.AddComponent<Rigidbody>();
+        //}
+
+        //var objects = explosionParent.GetComponentsInChildren<Rigidbody>();
+        //for(var i=0; i < objects.Length; i++)
+        //{
+        //    //UnityEngine.Gizmos.DrawSphere(gameObject.transform.position, 50);
+        //    objects[i].AddExplosionForce(explosionForce, explosionOrigin.position, 50);
+        //}
+        Destroy(gameObject,3);
+
+    }
+    private void UnparentSplitScreen()
+    {
+        splitScreenGameObject.transform.parent = null;
+
+        var ss = splitScreenGameObject.GetComponent<SplitScreenEffect>();
+        ss.Screens.ForEach(s =>
+        {
+            s.Camera.transform.parent = null;
+            s.Target.transform.parent = null;
+        });
     }
 }
