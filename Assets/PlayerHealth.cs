6190f0e3f3c20329ddb847fd268f5f6efe8a43de
jcianci12 <jcianci12@gmail.com>
2023-01-11 20:20:27 +1000

    update

--- Assets/PlayerHealth.cs
+++ Assets/PlayerHealth.cs
@@ -4,19 +4,27 @@ using UnityEngine;
 
 public class PlayerHealth : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public float health;
+    public PlayerController controller;
+    public Vector3 spawnLocation;
 
-    // Update is called once per frame
-    void Update()
+    //we need a health bar
+
+    //we need a way to adjust the health bar
+
+
+
+    // Start is called before the first frame update
+    public void Start()
     {
-        
+        spawnLocation = GameObject.Find ("Spawn").transform.position;
     }
     public void TakeDamage(int damage)
     {
-
+        health -= damage;
+        if(health < 0)
+        {
+            controller.transform.position = spawnLocation;
+        }
     }
 }
